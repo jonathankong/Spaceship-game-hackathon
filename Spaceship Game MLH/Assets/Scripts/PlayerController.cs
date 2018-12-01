ee2a0e16c3d94db8833fff030828e6ed1940c6d3
RaymanRJ <34038866+RaymanRJ@users.noreply.github.com>
2018-12-01 11:39:53 -0500

    Added a Circle

---
+++ Spaceship Game MLH/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame - before rendering
+	void Update () {
+		
+	}
+
+    // Called before Physics updates
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+
+        rb.AddForce(movement);
+    }
+}
