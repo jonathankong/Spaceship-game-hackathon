25ea176c19c5f4a5ecb2195b98a46dd0dfe556a1
RaymanRJ <34038866+RaymanRJ@users.noreply.github.com>
2018-12-01 13:41:45 -0500

    Rohan did everything

--- Spaceship Game MLH/Assets/Scripts/Mover.cs
+++ Spaceship Game MLH/Assets/Scripts/Mover.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public float speed;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.forward * speed;
+        Destroy(this.gameObject, 2f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		
-	}
+        rb.velocity = transform.right * speed;
+
+    }
 }
--- Spaceship Game MLH/Assets/Scripts/PlayerController.cs
+++ Spaceship Game MLH/Assets/Scripts/PlayerController.cs
@@ -11,17 +11,19 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour {
 
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public Boundary boundary;
+    public Transform shootPoint;
+    public GameObject bullet;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(shoot());
 	}
-	
-	// Update is called once per frame - before rendering
-	void Update () {
-		
+
+    // Update is called once per frame - before rendering
+    void Update() {
 	}
 
     // Called before Physics updates
@@ -39,5 +41,13 @@ public class PlayerController : MonoBehaviour {
             Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
             0.0f
         );
+    }
+
+    IEnumerator shoot()
+    {
+        while (true) { 
+            yield return new WaitForSeconds(0.25f);
+            Instantiate(bullet, shootPoint.position, Quaternion.identity);
+        }
     }
 }
