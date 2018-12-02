dc628511ba4893230d8ca1e4042391f03d03fa1e
jonathan.ong <jko665_000@msn.com>
2018-12-01 20:36:32 -0500

    Code Overhaul

--- Spaceship Game MLH/Assets/Scripts/EnemyScript.cs
+++ Spaceship Game MLH/Assets/Scripts/EnemyScript.cs
@@ -12,15 +12,14 @@ public class EnemyScript : MonoBehaviour
     /// <summary>
     /// Object speed
     /// </summary>
-    public Vector2 speed = new Vector2(10, 10);
-    public float frequency = 20f;
-    public float magnitude = 0.5f;
+    public Vector2 speed = new Vector2(5, 5);
+    public Vector2 direction = new Vector2(-1, 0);
+    public float frequency = 5f;
+    public float magnitude = 10f;
 
     /// <summary>
     /// Moving direction
     /// </summary>
-    public Vector2 direction = new Vector2(-1, 0);
-
     private Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
 
@@ -30,7 +29,7 @@ public class EnemyScript : MonoBehaviour
         movement = new Vector2(
           speed.x * direction.x,
           speed.y * direction.y + Mathf.Sin(Time.time * frequency) * magnitude);
-        transform.eulerAngles = new Vector3(0, 0, -Mathf.Sin(Time.time * frequency / 2) * 0.5f * 45);
+        transform.eulerAngles = new Vector3(0, 0, -Mathf.Sin(Time.time * frequency) * 0.5f * 45);
 
     }
 
